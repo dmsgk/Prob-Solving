f81b662bcc29c506e2fdf66691d7f4ffd7e6fcd3
johyeon yoon <yds03080@naver.com>
2021-06-12 14:41:16 +0900

    Add intersection_of_two_arrays.py

---
+++ leetcode/binary_search/intersection_of_two_arrays.py
@@ -0,0 +1,11 @@
+# 349. Intersection of Two Arrays
+
+# runtime : 44 ms	memory: 14.1 MB
+from typing import List
+
+
+class Solution:
+    def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        nums1 = set(nums1)
+        nums2 = set(nums2)
+        return list(nums1.intersection(nums2))
