7893f674cd0f2423bc70008aaa4879839618bd40
johyeon yoon <yds03080@naver.com>
2021-06-22 17:24:37 +0900

    Fix house_robber.py

--- leetcode/dynamic_programming/house_robber.py
+++ leetcode/dynamic_programming/house_robber.py
@@ -1,18 +1,19 @@
 # 198. House Robber
 from typing import List
+import copy
 
 
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        ans = [0,0]
+        dp = copy.deepcopy(nums)
 
-        for i, v in enumerate(nums):
-            if i % 2 == 1:
-                ans[0] += v
-            else:
-                ans[1] += v
+        idx = 2
+        while idx < len(nums):
+            dp[idx] = dp[idx] + max(dp[:idx - 1])
+            idx += 1
 
-        return max(ans)
+        return max(dp)
+
 
 test = Solution()
 print(test.rob([2,1,1,2]))  # 4
