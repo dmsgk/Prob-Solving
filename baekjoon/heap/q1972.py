0a242cb40efb4c84a262b644b9be7be4ee52bd0e
johyeon yoon <yds03080@naver.com>
2021-07-08 14:00:13 +0900

    Add q1972.py

---
+++ baekjoon/heap/q1972.py
@@ -0,0 +1,17 @@
+# 최소 힙
+
+import sys
+import heapq
+
+n = int(sys.stdin.readline().strip())
+queue = []
+
+for _ in range(n):
+    x = int(sys.stdin.readline().strip())
+    if x == 0:
+        if not queue:
+            print(0)
+        else:
+            print(heapq.heappop(queue))
+    else:
+        heapq.heappush(queue, x)
