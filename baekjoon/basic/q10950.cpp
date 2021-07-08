646caea8574e2dba125bd1b351dca710f8d49166
johyeon yoon <yds03080@naver.com>
2021-07-08 22:38:25 +0900

    Add q10950.cpp

---
+++ baekjoon/basic/q10950.cpp
@@ -0,0 +1,20 @@
+//
+//  q10950.cpp
+//  cppAlgorithm
+//
+//  Created by Johyeon Yoon on 2021/07/08.
+//
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    int T, A, B;
+    cin >> T;
+    for (int i = 0; i < T; i++){
+        cin >> A >> B;
+        cout << A+B << endl;
+    }
+    
+    return 0;
+}
