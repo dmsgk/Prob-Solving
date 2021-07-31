4ba856e2f95ef1d0daba35d8f778829dee348040
johyeon yoon <yds03080@naver.com>
2021-07-31 10:51:04 +0900

    Add q9372.py

---
+++ baekjoon/minimum_spanning_tree/q9372.py
@@ -0,0 +1,11 @@
+# 상근이의 여행
+
+import sys
+
+
+t = int(sys.stdin.readline().rstrip())
+for _ in range(t):
+    n, m = map(int, sys.stdin.readline().rstrip().split())
+    for _ in range(m):
+        a,b = map(int, sys.stdin.readline().rstrip().split())
+    print(n-1)
